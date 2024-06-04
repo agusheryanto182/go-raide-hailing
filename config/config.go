314954037b93712f3f169a4acad097b1f6e65d76
Agus Heryanto <agusheryantodev@gmail.com>
2024-06-04 09:03:02 +0700

    update

--- config/config.go
+++ config/config.go
@@ -1,85 +1,47 @@
 package config
 
 import (
-	"fmt"
-	"net/url"
-	"time"
+	"log"
 
-	"github.com/spf13/viper"
+	"github.com/joeshaw/envdecode"
 )
 
 type Global struct {
-	ServerHost string
-	ServerPort int
-
-	DbString       string
-	DbMaxOpenConns int
-	// DbMaxIdleConns    int
-	DbMaxConnIdleTime time.Duration
-	// DbMaxConnLifetime time.Duration
-
-	JwtSecretKey   string
-	BcryptSaltCost int
-
-	AwsAccessKeyID     string
-	AwsSecretAccessKey string
-	AwsS3BucketName    string
-	AwsRegion          string
-
-	LogLevel string
+	Database Database
+	Jwt      Jwt
+	Bcrypt   Bcrypt
+	AWS      AWS
 }
 
-func LoadConfig() (cfg Global, err error) {
-	conf := viper.New()
-	conf.SetConfigFile(".env")
-	conf.SetConfigType("env")
-	conf.AutomaticEnv()
+type Bcrypt struct {
+	Salt int `env:"BCRYPT_SALT,required"`
+}
 
-	err = conf.ReadInConfig()
-	if err != nil {
-		return
+type Jwt struct {
+	Secret string `env:"JWT_SECRET,required"`
+}
+
+type Database struct {
+	Username string `env:"DB_USERNAME,required"`
+	Password string `env:"DB_PASSWORD,required"`
+	Host     string `env:"DB_HOST,required"`
+	Port     string `env:"DB_PORT,required"`
+	DbName   string `env:"DB_NAME,required"`
+	Params   string `env:"DB_PARAMS,required"`
+}
+
+type AWS struct {
+	ID         string `env:"AWS_ACCESS_KEY_ID,required"`
+	SecretKey  string `env:"AWS_SECRET_ACCESS_KEY,required"`
+	BucketName string `env:"AWS_S3_BUCKET_NAME,required"`
+	Region     string `env:"AWS_REGION,required"`
+}
+
+func NewConfig() *Global {
+	var c Global
+	if err := envdecode.StrictDecode(&c); err != nil {
+		log.Fatalf("Failed to decode: %s", err)
 	}
 
-	conf.SetDefault("HOST", "0.0.0.0")
-	conf.SetDefault("PORT", 8080)
-
-	cfg.ServerHost = conf.GetString("HOST")
-	cfg.ServerPort = conf.GetInt("PORT")
-
-	cfg.DbString = fmt.Sprintf(
-		"postgres://%s:%s@%s:%d/%s?%s",
-		conf.GetString("DB_USERNAME"),
-		url.QueryEscape(conf.GetString("DB_PASSWORD")),
-		conf.GetString("DB_HOST"),
-		conf.GetInt("DB_PORT"),
-		conf.GetString("DB_NAME"),
-		conf.GetString("DB_PARAMS"),
-	)
-
-	conf.SetDefault("DB_MAX_OPEN_CONNS", 100)
-	cfg.DbMaxOpenConns = conf.GetInt("DB_MAX_OPEN_CONNS")
-
-	// conf.SetDefault("DB_MAX_IDLE_CONNS", 25)
-	// cfg.DbMaxIdleConns = conf.GetInt("DB_MAX_IDLE_CONNS")
-
-	conf.SetDefault("DB_MAX_CONN_IDLE_TIME_S", 5)
-	cfg.DbMaxConnIdleTime = time.Duration(
-		conf.GetInt64("DB_MAX_CONN_IDLE_TIME_S"),
-	) * time.Second
-
-	// conf.SetDefault("DB_MAX_CONN_LIFETIME_MS", 0)
-	// cfg.DbMaxConnLifetime = time.Duration(
-	// 	conf.GetInt64("DB_MAX_CONN_LIFETIME_MS"),
-	// ) * time.Millisecond
-
-	cfg.JwtSecretKey = conf.GetString("JWT_SECRET")
-	cfg.BcryptSaltCost = conf.GetInt("BCRYPT_SALT")
-
-	cfg.AwsAccessKeyID = conf.GetString("AWS_ACCESS_KEY_ID")
-	cfg.AwsSecretAccessKey = conf.GetString("AWS_SECRET_ACCESS_KEY")
-	cfg.AwsS3BucketName = conf.GetString("AWS_S3_BUCKET_NAME")
-	cfg.AwsRegion = conf.GetString("AWS_REGION")
-
-	cfg.LogLevel = conf.GetString("LOG_LEVEL")
-	return
+	return &c
 }
--- go.mod
+++ go.mod
@@ -13,8 +13,8 @@ require (
 	github.com/google/uuid v1.5.0
 	github.com/jackc/pgx/v4 v4.18.3
 	github.com/jmoiron/sqlx v1.4.0
+	github.com/joeshaw/envdecode v0.0.0-20200121155833-099f1fc765bd
 	github.com/phsym/console-slog v0.3.1
-	github.com/spf13/viper v1.18.2
 	golang.org/x/crypto v0.21.0
 )
 
@@ -34,11 +34,10 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.24.3 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.28.10 // indirect
 	github.com/aws/smithy-go v1.20.2 // indirect
-	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.14.3 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
@@ -48,29 +47,16 @@ require (
 	github.com/jackc/pgtype v1.14.0 // indirect
 	github.com/klauspost/compress v1.17.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
-	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
-	github.com/mitchellh/mapstructure v1.5.0 // indirect
-	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
-	github.com/sagikazarmark/locafero v0.4.0 // indirect
-	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
-	github.com/sourcegraph/conc v0.3.0 // indirect
-	github.com/spf13/afero v1.11.0 // indirect
-	github.com/spf13/cast v1.6.0 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	go.uber.org/atomic v1.9.0 // indirect
-	go.uber.org/multierr v1.9.0 // indirect
-	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
 	golang.org/x/net v0.23.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
-	gopkg.in/ini.v1 v1.67.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
